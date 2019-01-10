41bd70da85cb08d5306c221b76c2d99ee562a291
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-01-10 19:01:15 +0200

    10_01_2019_01

--- public/js/work.js
+++ public/js/work.js
@@ -1,9 +1,14 @@
+
+'use strict'
+
 var log = console.log;
+
 
 const reqButton = document.querySelector('#sendReq')
 const timeStampS = document.querySelector('#timeStampS');
 const timeStampF = document.querySelector('#timeStampF');
 const dataTable = document.querySelector('#dataFromDB > tbody');
+const periodChoice = document.querySelectorAll('#periodSet > input');
 
 const headers = {
     'Accept': 'application/json',
@@ -11,36 +16,83 @@ const headers = {
 };
 
 
+
+
+const forDateChoise = document.querySelector('.forDateChoise > tbody > tr');
+//log(forDateChoise);
+
+
+
+
+//timeStampS.addEventListener('change', );
+//timeStampF.addEventListener('change', periodValidator);
+
+function setPeriod(periodCheck) {
+    for (let period of periodChoice) { log(period, periodCheck)
+        if (period.value !== periodCheck) period.checked = false
+        else period.checked = true;
+    };
+};
+
+function periodValidator() {
+
+    if (timeStampS.value > timeStampF.value) {
+        timeStampS.value = timeStampF.value;
+        setPeriod('day');
+    };
+
+    if ((timeStampF.valueAsDate - timeStampS.valueAsDate) < 2678400000) {
+       // periodChoice[0].checked = false;
+       // periodChoice[1].checked = true;
+    };
+
+
+    log(timeStampS.value, timeStampF.value);
+};
+
+
+
+
+forDateChoise.addEventListener('change', periodValidator)
+forDateChoise.addEventListener('click', periodValidator)
+
+
+function getChoicePeriod(periodCheck) {
+    for (let period of periodCheck) {
+        if (period.checked) return period.value;
+    };
+
+};
+
 reqButton.addEventListener('click', async () => {
     try {
         let TimeStamp = {
             timeStart: timeStampS.value,
-            timeFinish: timeStampF.value
+            timeFinish: timeStampF.value,
+            period: getChoicePeriod(periodChoice)
         };
         const rawResponse = await fetch('http://localhost:3000/apidb', {
             method: 'POST',
             headers,
             body: JSON.stringify({ TimeStamp })
         });
+        // log(TimeStamp);
         const result = await rawResponse.json();
         if (result) {
-            result.map((result) => log('RESULT', result));
-
-            result.map((rowResult) => {
-
-                dataTable.innerHTML += `
-                    <tr>
-                        <td> ${rowResult.CH1} </td>
-                        <td> ${rowResult.CH2} </td>
-                        <td> ${rowResult.TIMEPOINT} </td>
-                        <td> ${rowResult.SERIAL} </td>
-                    </tr>
+            log('RESULT', result);
+            /*
+                        result.map((rowResult) => {
             
-            
-            `;
-            });
-
-
+                            dataTable.innerHTML += `
+                                <tr>
+                                    <td> ${rowResult.CH1} </td>
+                                    <td> ${rowResult.CH2} </td>
+                                    <td> ${rowResult.TIMEPOINT} </td>
+                                    <td> ${rowResult.SERIAL} </td>
+                                </tr>                    
+                                                `;
+                        });
+            */
 
         };
     }
--- routes/apidb.js
+++ routes/apidb.js
@@ -13,24 +13,79 @@ options.pageSize = 4096;
 options.role = 'ADMIN';
 
 
-
-
-
 router.post('/', async (req, res, next) => {
 
-    let scriptGETDATA = " SELECT * FROM COUNTERDATA WHERE TIMEPOINT >= " + "'" + req.body.TimeStamp.timeStart + "'" + " AND TIMEPOINT <= " + "'" + req.body.TimeStamp.timeFinish + "'";
+    log(forPaintWeekend(req.body.TimeStamp.timeStart));
+    selectionFromDB(req.body.TimeStamp);
+
+
+    /*
+    let scriptGETDATA = " SELECT * FROM COUNTERDATA WHERE TIMEPOINT >= " + "'" + tryDateTimeStamp + "'" + " AND TIMEPOINT <= " + "'" + req.body.TimeStamp.timeFinish + "'";
     // let scriptGETSUM = " SELECT SUM(CH1) FROM COUNTERDATA WHERE  (CAST(TIMEPOINT AS DATE) >= " + "'"+ req.body.TimeStamp.timeStart + "'" + ") AND (CAST(TIMEPOINT AS DATE) <= " + "'"+ req.body.TimeStamp.timeFinish + "'" + ") AND CH1 = CH2 ";
-    firebird.attach(options, function (err, db) {
+    firebird.attach(options, (err, db) => {
         if (err) res.json(err)
-        else log("ATTACHED");
-        db.query(scriptGETDATA, function (err, result) {
+        //else log("ATTACHED");
+        db.query(scriptGETDATA, (err, result) => {
             if (err) log(err);
             db.detach();
-            log("DETACHED");
-            result.map((result) => log('RESULT = ', result));
-            res.json(result);
+            //log("DETACHED");
+            //result.map((result) => log('RESULT = ', result));
+          //  res.json(result);
         });
     });
+*/
+
 });
+
+function forPaintWeekend(date) {
+    d = new Date(date);
+    if (d.getDay() === 0 || d.getDay() === 6) return true
+    else return false;
+};
+
+
+function selectionFromDB(timePoints) {
+
+    log(timePoints.period);
+    let dateStart = new Date(timePoints.timeStart);
+    let dateFinish = new Date(timePoints.timeStart);
+
+
+
+    let month = dateStart.getMonth() + 1;
+
+
+    let timeS = dateStart.getFullYear() + '-' + month + '-' +
+        dateStart.getDate() + ' ' + dateStart.getUTCHours() + ':' +
+        dateStart.getMinutes() + ':' + dateStart.getSeconds();
+
+
+
+
+    //let tryDateTimeStamp = req.body.TimeStamp.timeStart + ' 9:0:0';
+    //log('TimeStamp= ', tryDateTimeStamp);
+    log('***********************');
+    // log('timeStart= ', req.body.TimeStamp.timeStart);
+    log('DATE:', timeS);
+    log('***********************');
+
+    let scriptGETSUM = " SELECT SUM(CH1) FROM COUNTERDATA WHERE (CAST(TIMEPOINT AS TIMESTAMP) >= " + "'"
+        + timeS + "'" + ") AND (CAST(TIMEPOINT AS TIMESTAMP) <= " + "'"
+        + timePoints.timeFinish + "'" + ") AND CH1 = CH2 ";
+        
+    firebird.attach(options, (err, db) => {
+        if (err) res.json(err)
+        else //log("ATTACHED");
+            db.query(scriptGETSUM, (err, result) => {
+                if (err) log(err);
+                db.detach();
+                //log("DETACHED");
+                log(result);
+                // res.json(result);
+            });
+    });
+
+}
+
 
 module.exports = router;
