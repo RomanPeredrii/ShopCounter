c1931477051bb200e5d2ff36b6b5f6965d995ca4
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-01-15 16:30:46 +0200

    15_01_2019_1

--- public/js/work.js
+++ public/js/work.js
@@ -3,13 +3,13 @@
 
 var log = console.log;
 
-const reqButton = document.querySelector('#sendReq')
+const divSendReq = document.querySelector('.divSendReq');
+const reqButton = document.querySelector('#sendReq');
 const timeStampS = document.querySelector('#timeStampS');
 const timeStampF = document.querySelector('#timeStampF');
 const dataTable = document.querySelector('#dataFromDB > tbody');
-const periodChoice = document.querySelectorAll('#periodSet > input');
 const divPeriodSet = document.querySelector('#periodSet');
-
+const forDateChoise = document.querySelector('.forDateChoise > tbody > tr');
 
 const headers = {
     'Accept': 'application/json',
@@ -17,88 +17,81 @@ const headers = {
 };
 
 
+function setPeriod(set) {
+    let period = ["day", "week", "month"];
+    let inputs = '';
+    for (let i = 0; i <= set; i++) {
+        // log(i);
+        inputs += `
+            <input id="period${period[i]}" type="radio" name="period" value= ${period[i]} />
+            <label for="period${period[i]}"> ${period[i]} </label>
+         `};
+    divPeriodSet.innerHTML = inputs;
 
-
-//const forDateChoise = document.querySelector('.forDateChoise > tbody > tr');
-//log(forDateChoise);
-
-//timeStampS.addEventListener('change', );
-//timeStampF.addEventListener('change', periodValidator);
-
-function blockPeriod(periodCheck) {
-    for (let period of periodChoice) {
-        if (period.value === periodCheck) period.checked = false;
-
-    };
-};
-
-
-function setPeriod(periodCheck) {
-    for (let period of periodChoice) {
-        if (period.value !== periodCheck) period.checked = false;
-        else period.checked = true;
-    };
 };
 
 function periodValidator() {
 
-    if ((timeStampF.valueAsDate - timeStampS.valueAsDate) < 2678400000) {
-        blockPeriod('month');
-        if ((timeStampF.valueAsDate - timeStampS.valueAsDate) < 604800000) {
-            blockPeriod('week');
-            setPeriod('day');
-            if (timeStampS.value >= timeStampF.value) {
-                timeStampS.value = timeStampF.value;
-                setPeriod('day');
-            };
-        };
+    let period = timeStampF.valueAsDate - timeStampS.valueAsDate;
+    let month = 2678400000;
+    let week = 604800000;
+
+    if (period > month) setPeriod(2);
+    if (period < month) setPeriod(1);
+    if (period < week) setPeriod(0);
+    if (timeStampS.value >= timeStampF.value) {
+        timeStampS.value = timeStampF.value;
+        setPeriod(0);
     };
 
-//    log(timeStampS.value, timeStampF.value);
+
 };
 
-divPeriodSet.addEventListener('click', periodValidator);
-divPeriodSet.addEventListener('change', periodValidator);
-//forDateChoise.addEventListener('change', periodValidator);
-//forDateChoise.addEventListener('click', periodValidator);
+forDateChoise.addEventListener('click', () => divSendReq.style.display = 'none');
+divPeriodSet.addEventListener('click', () => divSendReq.style.display = 'block');
+forDateChoise.addEventListener('change', periodValidator);
+forDateChoise.addEventListener('click', periodValidator);
 
 
 function getChoicePeriod(periodCheck) {
     for (let period of periodCheck) {
         if (period.checked) return period.value;
     };
-
 };
 
+
+
 reqButton.addEventListener('click', async () => {
-    try {
-        let TimeStamp = {
-            timeStart: timeStampS.value,
-            timeFinish: timeStampF.value,
-            period: getChoicePeriod(periodChoice)
-        };
-        const rawResponse = await fetch('http://localhost:3000/apidb', {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({ TimeStamp })
-        });
-        // log(TimeStamp);
-        const result = await rawResponse.json();
-        if (result) {
-            log('RESULT', result);
-            
-                        result.map((rowResult) => {
-            
-                            dataTable.innerHTML += `
+    const periodChoice = document.querySelectorAll('#periodSet > input');
+    if (!getChoicePeriod(periodChoice)) periodChoice[0].checked = true;  // захист від дурнів
+    else {
+        try {
+            let TimeStamp = {
+                timeStart: timeStampS.value,
+                timeFinish: timeStampF.value,
+                period: getChoicePeriod(periodChoice)
+            };
+            const rawResponse = await fetch('http://localhost:3000/apidb', {
+                method: 'POST',
+                headers,
+                body: JSON.stringify({ TimeStamp })
+            });
+            // log(TimeStamp);
+            const result = await rawResponse.json();
+            if (result) {
+                log('RESULT', result);
+
+                result.map((rowResult) => {
+
+                    dataTable.innerHTML += `
                                 <tr>
                                     <td> ${rowResult.SUM} </td>
                                     
                                 </tr>                    
                                                 `;
-                        });
-            
-
-        };
-    }
-    catch (err) { log(err) };
+                });
+            };
+        }
+        catch (err) { log(err) };
+    };
 });
--- routes/apidb.js
+++ routes/apidb.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var firebird = require('node-firebird');
 var options = {};
 
+// OPTION FOR ATTACH DB
 options.host = 'localhost';
 options.port = 3050;
 options.database = 'D:/JS/vvv/COUNTER.FDB';
@@ -19,72 +20,54 @@ router.post('/', async (req, res, next) => {
     selectionFromDB(req.body.TimeStamp);
 
 
-    /*
-    let scriptGETDATA = " SELECT * FROM COUNTERDATA WHERE TIMEPOINT >= " + "'" + tryDateTimeStamp + "'" + " AND TIMEPOINT <= " + "'" + req.body.TimeStamp.timeFinish + "'";
-    // let scriptGETSUM = " SELECT SUM(CH1) FROM COUNTERDATA WHERE  (CAST(TIMEPOINT AS DATE) >= " + "'"+ req.body.TimeStamp.timeStart + "'" + ") AND (CAST(TIMEPOINT AS DATE) <= " + "'"+ req.body.TimeStamp.timeFinish + "'" + ") AND CH1 = CH2 ";
-    firebird.attach(options, (err, db) => {
-        if (err) res.json(err)
-        //else log("ATTACHED");
-        db.query(scriptGETDATA, (err, result) => {
-            if (err) log(err);
-            db.detach();
-            //log("DETACHED");
-            //result.map((result) => log('RESULT = ', result));
-          //  res.json(result);
+    function forPaintWeekend(date) {
+        d = new Date(date);
+        if (d.getDay() === 0 || d.getDay() === 6) return true
+        else return false;
+    };
+
+    // 
+    function selectionFromDB(timePoints) {
+        log('TIMEPOINTS:', timePoints);
+        log('PERIOD:', timePoints.period);
+        let dateStart = new Date(timePoints.timeStart);
+        let dateFinish = new Date(timePoints.timeFinish + ' 23:59:59:999');
+
+        let timeS = dateStart.getFullYear() + '-' + (dateStart.getMonth() + 1) + '-' +
+            dateStart.getDate() + ' ' + dateStart.getUTCHours() + ':' +
+            dateStart.getMinutes() + ':' + dateStart.getSeconds();
+
+        let timeF = dateFinish.getFullYear() + '-' + (dateFinish.getMonth() + 1) + '-' +
+            dateFinish.getDate() + ' ' + dateFinish.getHours() + ':' +
+            dateFinish.getMinutes() + ':' + dateFinish.getSeconds();
+
+
+        log('***********************');
+        log('START:', timeS);
+        log('FINISH:', timeF);
+        log('***********************');
+
+
+        // GENERATING SQL SCRIPT
+        let scriptGETSUM = " SELECT SUM(CH1) FROM COUNTERDATA WHERE (CAST(TIMEPOINT AS TIMESTAMP) >= " + "'"
+            + timeS + "'" + ") AND (CAST(TIMEPOINT AS TIMESTAMP) <= " + "'"
+            + timeF + "'" + ") AND CH1 = CH2 ";
+
+
+        // REQUEST DB
+        firebird.attach(options, (err, db) => {
+            if (err) res.json(err)
+            else //log("ATTACHED");
+                db.query(scriptGETSUM, (err, result) => {
+                    if (err) log(err);
+                    db.detach();
+                    //log("DETACHED");
+                    log(result);
+                    res.json(result);
+                });
         });
-    });
-*/
 
-
-
-function forPaintWeekend(date) {
-    d = new Date(date);
-    if (d.getDay() === 0 || d.getDay() === 6) return true
-    else return false;
-};
-
-
-function selectionFromDB(timePoints) {
-    log('TIMEPOINTS:', timePoints);
-    log('PERIOD:', timePoints.period);
-    let dateStart = new Date(timePoints.timeStart);
-    let dateFinish = new Date(timePoints.timeFinish + ' 23:59:59:999');
-
-
-
-    let timeS = dateStart.getFullYear() + '-' + (dateStart.getMonth() + 1) + '-' +
-        dateStart.getDate() + ' ' + dateStart.getUTCHours() + ':' +
-        dateStart.getMinutes() + ':' + dateStart.getSeconds();
-
-    let timeF = dateFinish.getFullYear() + '-' + (dateFinish.getMonth() + 1) + '-' +
-        dateFinish.getDate() + ' ' + dateFinish.getHours() + ':' +
-        dateFinish.getMinutes() + ':' + dateFinish.getSeconds();
-
-
-    log('***********************');
-    log('START:', timeS);
-    log('FINISH:', timeF);
-    log('***********************');
-
-
-
-    let scriptGETSUM = " SELECT SUM(CH1) FROM COUNTERDATA WHERE (CAST(TIMEPOINT AS TIMESTAMP) >= " + "'"
-        + timeS + "'" + ") AND (CAST(TIMEPOINT AS TIMESTAMP) <= " + "'"
-        + timeF + "'" + ") AND CH1 = CH2 ";
-
-    firebird.attach(options, (err, db) => {
-        if (err) res.json(err)
-        else //log("ATTACHED");
-            db.query(scriptGETSUM, (err, result) => {
-                if (err) log(err);
-                db.detach();
-                //log("DETACHED");
-                log(result);
-                res.json(result);
-            });
-    });
-
-}
+    }
 });
 
 module.exports = router;
