dc8a6ac1d21d73a7fae631c4fc8a24bdaeb868e3
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-10-04 00:03:31 +0300

    Delete check.js

--- public/js/check.js
+++
@@ -1,9 +0,0 @@
-const adminOptions = {
-    host: '185.255.96.123',
-    port: ' 3050',
-    database: '/var/lib/firebird/2.5/data/counter.fdb',
-    user: 'U_VIEW',
-    password: 'clv8bzg1'
-};
-
-export default adminOptions;
