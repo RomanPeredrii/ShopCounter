931833c9cb4f2b6c77b31beb291ef3468bf6e508
RomanPeredrii <roman.peredrii@gmail.com>
2019-12-01 18:44:28 +0200

    environment prepared

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('node tsc 0');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = process.env.PORT || 3000;
+const app = express_1.default();
+app.get('/', (param) => 
+//@ts-ignore
+param.res.status(200).
+    json({
+    message: 'Running'
+}));
+app.listen(PORT, () => console.log(`Using ${PORT} port`));
