267ae55326e6512260ecaa4f64cbdd3c01802a49
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-12-03 18:38:33 +0200

    feat: login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,35 +1,51 @@
 import User from "../models/Users";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { jwtString } from '../config/keys';
 
-export function login(req: any, res: any) {
-  res.status(200).json({
-    login: {
-      username: req.body.username,
-      password: req.body.password
-    }
-  });
-}
+export async function login(req: any, res: any) {
+  const candidate = await User.findOne({ username: req.body.username });
+  if (candidate) {
+    // @ts-ignore
+    const paswordOk = bcrypt.compareSync(req.body.password, candidate.password);
+    if (paswordOk) {     
+      const token: string = jwt.sign({
+        // @ts-ignore
+        username: candidate.username,
+        userId: candidate._id
+      }, 
+      jwtString, 
+      {expiresIn: 3600});
+      res.status(200).json({token: `Bearer ${token}`});
+    } else {
+      res.status(401).json({ message: `password incorrect, try again` });
+    };
+  } else {
+    res
+      .status(404)
+      .json({ message: `user with name ${req.body.username} not exists` });
+  };
+};
 
 export async function registration(req: any, res: any) {
   const candidate = await User.findOne({ username: req.body.username });
   if (candidate) {
     res
       .status(409)
-      .json({ message: `user with name ${req.body.username} exist` });
+      .json({ message: `user with name ${req.body.username} exists` });
   } else {
+    const salt = bcrypt.genSaltSync(10);
     const user = new User({
       username: req.body.username,
-      password: req.body.password
+      password: bcrypt.hashSync(req.body.password, salt)
     });
-    try { 
-        await user.save();
-        res
-      .status(201)
-      .json({ message: user });
-  } catch (error) {
-    res
-    .status(500)
-    .json({ message: `internal server error` });
-  }};
+    try {
+      await user.save();
+      res.status(201).json({ message: user });
+    } catch (error) {
+      res.status(500).json({ message: `internal server error` });
+    };
+  };
 };
 
 export function remove(req: any, res: any) {
