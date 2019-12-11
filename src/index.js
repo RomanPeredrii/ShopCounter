a94b38af48f81a22ff4c7b7c1e964a8a2a5e0d82
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-12-11 19:29:27 +0200

    fix: auth passport-jwt

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const app = require('./app');
-// const http = require('http')
-// const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server on ${PORT} port`));
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import  { login, registration, remove } from '../controllers/auth'
+import  { login, registration, remove } from '../controllers/auth';
+import passport from 'passport';
 const router = express.Router();
 
-router.post('/login', login);
+// --> /api/auth
+
+router.post('/login',  login);
 router.post('/register', registration);
-router.delete('/delete', remove);
+router.delete('/delete/:username', passport.authenticate('jwt', {session: false}), remove);
 
 export default router;
